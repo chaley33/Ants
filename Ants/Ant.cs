129cf89f4af7a2eb566f08c4eadb172e503b173e
Chase Haley <c.haley3399@gmail.com>
2021-04-02 13:11:53 -0500

    Initial Commit. Ants show up and move (too much) each frame.

---
+++ Ants/Ant.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Ants
+{
+    class Ant
+    {
+        public Sprite Sprite { get; }
+        private float xScale = 0.05f;
+        private float yScale = 0.05f;
+
+        public Ant()
+        {
+            Texture texture =
+                new Texture("E:\\OneDrive - Tennessee Tech University\\Programming\\C#\\Ants\\Ants\\ant.png");
+
+            Sprite = new Sprite(texture);
+            Sprite.Scale = new Vector2f(xScale, yScale);
+
+            var rand = new Random();
+            var randX = rand.Next(200, 600);
+            var randY = rand.Next(200, 400);
+            Console.WriteLine($"{randX}, {randY}");
+            Sprite.Position = new Vector2f(randX, randY);
+        }
+
+        public Ant(int posX, int posY)
+        {
+            try
+            {
+                Texture texture =
+                    new Texture("E:\\OneDrive - Tennessee Tech University\\Programming\\C#\\Ants\\Ants\\ant.png");
+
+                Sprite = new Sprite(texture);
+                Sprite.Scale = new Vector2f(xScale, yScale);
+                Sprite.Position = new Vector2f(posX, posY);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\ne.ToString:\n {e.ToString()}\n\n");
+            }
+        }
+
+        public void Move()
+        {
+            var rand = new Random();
+            Sprite.Position = new Vector2f(Sprite.Position.X + rand.Next(-5, 6), Sprite.Position.Y + rand.Next(-5, 6));
+        }
+    }
+}
---
+++ Ants/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Ants
+{
+    class Program
+    {
+        private static List<Ant> _antsList;
+        public static RenderWindow window;
+        public const int WindowWidth = 800;
+        public const int WindowHeight = 600;
+        static void Main(string[] args)
+        {
+            _antsList = new List<Ant>();
+            GenerateAnts(10);
+            // var window = new Window(new VideoMode(800, 600), "My window");
+            window = new RenderWindow(new VideoMode(WindowWidth, WindowHeight), "My window");
+            window.Closed += new EventHandler(OnClose);
+            window.MouseButtonPressed += new EventHandler<MouseButtonEventArgs>(MouseButtonPressed);
+
+            while (window.IsOpen)
+            {
+                window.Clear();
+                foreach (var ant in _antsList)
+                {
+                    ant.Move();
+                    window.Draw(ant.Sprite);
+                }
+                window.DispatchEvents();
+                window.Display();
+            }
+        }
+
+        static void OnClose(object sender, EventArgs e)
+        {
+            Console.WriteLine("\n\nExiting...\n\n");
+            window.Close();
+        }
+
+        static void MouseButtonPressed(object sender, MouseButtonEventArgs mouse)
+        {
+            _antsList.Add(new Ant(mouse.X, mouse.Y));
+            Console.WriteLine("\nMouse Button Pressed!\n");
+        }
+
+        static void GenerateAnts(int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                _antsList.Add(new Ant());
+            }
+        }
+    }
+}
